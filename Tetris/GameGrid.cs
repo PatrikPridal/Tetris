75ae5378f1d5cb1731c73d435886e2028204d5cf
Patrik Přidal <98656826+vibemane@users.noreply.github.com>
2022-03-09 13:19:11 +0100

    Clear row

--- Tetris/GameGrid.cs
+++ Tetris/GameGrid.cs
@@ -37,7 +37,7 @@
             {
                 if (grid[r, c] == 0)
                 {
-                    return false
+                    return false;
                 }
             }
 
@@ -57,6 +57,23 @@
             return true;
         }
 
-        
+        private void ClearRow(int r)
+        {
+            for (int c = 0; c < Columns; c++)
+            {
+                grid[r, c] = 0;
+            }
+        }
+
+        private void MoveRowDown(int r, int numRows) 
+        {
+            for (int c = 0; c < Columns; c++)
+            {
+                grid[r + numRows, c] = grid[r, c];
+                grid[r, c] = 0;
+            }
+        }
+
+
     }
 }
