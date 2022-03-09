d32b7d330d870f54144ed0af0bad92b72467ed05
Patrik Přidal <98656826+vibemane@users.noreply.github.com>
2022-03-09 15:16:00 +0100

    gamestate

--- Tetris/GameState.cs
+++ Tetris/GameState.cs
@@ -2,6 +2,29 @@
 {
     public class GameState
     {
+        private Block currentBlock;
+
+        public Block CurrentBlock
+        {
+            get => currentBlock;
+            private set
+            {
+                currentBlock = value;
+                currentBlock.Reset();
+            }
+        }
+
+        public GameGrid GameGrid { get; }
+        public BlockQueue BlockQueue { get; }
+        public bool GameOver { get; private set; }
+
+        public GameState()
+        {
+            GameGrid = new GameGrid(22, 10);
+            BlockQueue = new BlockQueue();
+            CurrentBlock = BlockQueue.GetAndUpdate();
+        }
+
 
     }
 }
