445aa8e48038526c6bf8be78a88c036082f2c712
Patrik Přidal <98656826+vibemane@users.noreply.github.com>
2022-03-09 13:37:19 +0100

    public abstact class Block

---
+++ Tetris/Block.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace Tetris
+{
+    public abstract class Block
+    {
+        protected abstract Position[][] Tiles { get; }
+        protected abstract Position StartOffset { get; }
+        public abstract int Id { get; }
+
+        private int rotationState;
+        private Position offset;
+
+        public Block()
+        {
+            offset = new Position(StartOffset.Row, StartOffset.Column);
+        }
+    }
+}
