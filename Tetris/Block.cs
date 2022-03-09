5f5e24f498c5d0fe86f30fa57e4d19c48c0da6f0
Patrik Přidal <98656826+vibemane@users.noreply.github.com>
2022-03-09 13:47:51 +0100

    rotation block

--- Tetris/Block.cs
+++ Tetris/Block.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System.Collections.Generic;
 
 namespace Tetris
 {
@@ -15,5 +15,32 @@ namespace Tetris
         {
             offset = new Position(StartOffset.Row, StartOffset.Column);
         }
+
+        public IEnumerable<Position> TilePositions()
+        {
+            foreach (Position p in Tiles[rotationState])
+            {
+                yield return new Position(p.Row + offset.Row, p.Column + offset.Column);
+            }
+        }
+
+        public void RotateCW()
+        {
+            rotationState = (rotationState + 1) % Tiles.Length;
+        }
+
+        public void RotateCCW()
+        {
+            if (rotationState == 0)
+            {
+                rotationState = rotationState = Tiles.Length - 1;
+            }
+            else
+            {
+                rotationState--;
+            }
+        }
+
+        
     }
 }
